b7435e569244bdd80831637f03afe67613164efc
ChuKien15121998 <chuvankien151298@gmail.com>
2022-03-27 10:25:02 +0700

    Done

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double length = 1.0;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.length = height;
+    }
+
+    public Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return (width + length) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "A Rectangle with width="
+                + getWidth()
+                + " and length="
+                + getLength()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
